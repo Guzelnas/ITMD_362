f7a99e1d58d007fb1b8e0bbcf626170dbfdbc1cd
Guzelnas <gnasybullina@gmail.com>
2018-02-01 18:36:08 -0600

    copy site.js from production problem 1

---
+++ Project_One/site.js
@@ -0,0 +1,14 @@
+$('#contact-form').on("submit", function(e) {
+  var phone = $('#phone').val();
+  var phoneValid = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+  e.preventDefault();
+  if (!phoneValid.test(phone)) {
+    console.log('invalid phone');
+    $('#invalid').remove();
+    $('#contact-form').append('<li id="invalid">Phone must contain 10 digits!</li>');
+    return false;
+  } else {
+    $(this).remove();
+    $('body').append('Thank you!');
+  }
+});
