2f8645f6b5ce6bf7e2945635d62a6ad94e65522c
Guzelnas <gnasybullina@gmail.com>
2018-02-18 09:59:07 -0600

    switch name and mail val

--- site.js
+++ site.js
@@ -4,15 +4,15 @@ $('#contact-form').on("submit", function(e) {
   var emailValid = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
   var nameValid = /^[a-zA-Z\s]*$/;
   e.preventDefault();
-  if (!emailValid.test(email)) {
-    console.log('invalid email');
-    $('#invalid').remove();
-    $('#email-address').append('<li id="invalid">Please enter valid email!</li>');
-    return false;
-  } else if (!nameValid.test(name)) {
+  if (!nameValid.test(name)) {
     console.log('invalid name');
     $('#invalid').remove();
     $('#Name').append('<li id="invalid">Letters and spaces only!</li>');
+    return false;
+  } else if (!emailValid.test(email)) {
+    console.log('invalid email');
+    $('#invalid').remove();
+    $('#email-address').append('<li id="invalid">Please enter valid email!</li>');
     return false;
   } else {
     $(this).remove();
