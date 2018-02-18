97ae0291106618490b857f37d0e3b676a2786215
Guzelnas <gnasybullina@gmail.com>
2018-02-17 22:44:44 -0600

    change regulae exp for name

--- Project_One/site.js
+++ Project_One/site.js
@@ -2,7 +2,7 @@ $('#contact-form').on("submit", function(e) {
   var email = $('#email').val();
   var name = $('#name').val();
   var emailValid = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-  var nameValid = /^[a-zA-Z]+$/;
+  var nameValid = /^[a-zA-Z\s]*$/;  
   e.preventDefault();
   if (!emailValid.test(email)) {
     console.log('invalid email');
