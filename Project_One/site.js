122d756b333fdc531304e7c723dd7c48913b0657
Guzelnas <gnasybullina@gmail.com>
2018-02-17 22:26:17 -0600

    add name validation

--- Project_One/site.js
+++ Project_One/site.js
@@ -1,13 +1,22 @@
 $('#contact-form').on("submit", function(e) {
   var email = $('#email').val();
+  var name = $('#name').val();
   var emailValid = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+  var nameValid = /^[a-zA-Z]+$/;
   e.preventDefault();
   if (!emailValid.test(email)) {
     console.log('invalid email');
     $('#invalid').remove();
     $('#email-address').append('<li id="invalid">Please enter valid email!</li>');
     return false;
-  } else {
+  }
+  if (!nameValid.test(name)) {
+    console.log('invalid name');
+    $('#invalid').remove();
+    $('#Name').append('<li id="invalid">Letters only!</li>');
+    return false;
+  }
+   else {
     $(this).remove();
     $('submit').append('Thank you!');
   }
